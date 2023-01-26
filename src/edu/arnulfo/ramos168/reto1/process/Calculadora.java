d6aebb8b60484dc14e741ec08c95149dbbfd458e
Arnulfo-Ramos168 <arrsaurio@gmail.com>
2023-01-26 10:18:39 -0600

    HI

---
+++ src/edu/arnulfo/ramos168/reto1/process/Calculadora.java
@@ -0,0 +1,16 @@
+package edu.arnulfo.ramos168.reto1.process;
+
+public class Calculadora {
+    /**
+     *
+     * suma enteros
+     *
+     * @param operando1 primer operando para sumar debe ser entero
+     * @param operando2 segundo operando pata sumar debe ser entero
+     */
+    public static int sumarNumeros(int operando1,int operando2){
+        return operando1+operando2;
+    }
+
+
+}
--- src/edu/arnulfo/ramos168/reto1/ui/CLI.java
+++ src/edu/arnulfo/ramos168/reto1/ui/CLI.java
@@ -1,4 +1,35 @@
 package edu.arnulfo.ramos168.reto1.ui;
 
+import edu.arnulfo.ramos168.reto1.process.Calculadora;
+
+import java.util.Scanner;
+
 public class CLI {
+    public static void mostrarMenu(){
+        System.out.println("Elige una opción");
+        System.out.println("(1) suma");
+        System.out.println("(2) resta");
+        System.out.println("(3) multi");
+        System.out.println("(4) divi");
+
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();scanner.nextLine();
+
+        System.out.println("introduce el primer valor");
+        int operando1 = scanner.nextInt();scanner.nextLine();
+        System.out.println("Introduce el segundo valor");
+        int operando2 =scanner.nextInt();scanner.nextLine();
+        int resultado=-1;
+        String operacion = "";
+        switch (option){
+            case 1:
+                Calculadora.sumarNumeros(operando1, operando2);
+                resultado = Calculadora.sumarNumeros(operando1, operando2);
+                break;
+
+        }
+        System.out.printf("El resultado de la operacion de %d + %d = %d ", operando1, operando2, resultado);
+
+
+    }
 }
